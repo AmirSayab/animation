a8d37aacfc30144ca2349973ae805238febdd410
AmirSayab <111190828+AmirSayab@users.noreply.github.com>
2023-03-21 00:00:19 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Cube from './components/Cube';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Cube />
+      <div>
+        <Footer />
+        </div >
+      </div>
+      );
+}
+
+      export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+
+
+export default function Footer() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline w-[200px]">
+                Hello world!
+            </h1>
+        </>
+    )
+}
